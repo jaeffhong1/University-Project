fd54329953eb11696d0c703fc0f2c16f5de6b85b
jaeff <jaeff.hong@education.nsw.gov.au>
2022-03-08 21:06:05 +1100

    Scrapy now only adds news if it isnt already in posts.json

--- PHASE_1/projscrape/projscrape/spiders/main_spider.py
+++ PHASE_1/projscrape/projscrape/spiders/main_spider.py
@@ -1,8 +1,14 @@
 import scrapy
 import re
+import json
+import os
+from json.decoder import JSONDecodeError
 
 class PostsSpider(scrapy.Spider):
     name = "posts"
+    # The variable that will hold the contents of posts.json
+    current_posts = ""
+    path_to_json = ""
 
     start_urls = [
         'https://www.cidrap.umn.edu/news-perspective'
@@ -25,8 +31,17 @@ class PostsSpider(scrapy.Spider):
         }[month]
 
     def parse(self, response):
-
         # For each date, loop through each article that was published on that date.
+
+        # Open up posts.json and copy its contents into current_posts.
+        self.path_to_json = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'posts.json')
+        with open(self.path_to_json) as news_posts:
+            try:
+                self.current_posts = json.load(news_posts)
+                news_posts.close()
+            except JSONDecodeError:
+                pass
+
         for date_published in response.css('div.views-set'):
             date = date_published.css('span.date-display-single::text').get()
 
@@ -37,22 +52,24 @@ class PostsSpider(scrapy.Spider):
 
             # Turn date into the correct format of year:month:day hour:minutes:seconds (hour:minutes:seconds is xx:xx:xx because I could not find it)
             date = str(year) + "-" + str(month) + "-" + str(day) + " xx:xx:xx"
-
+            
             for post in date_published.css('div.fieldlayout-region-body.fieldlayout-region-body-teaser'):
                 url = post.css('h3 a::attr(href)').get()
                 headline = post.css('h3 a::text').get()
-                main_text = post.css('div.field.field-name-field-teaser.field-type-text-long.field-label-hidden div.field-items div.field-item.even p::text').get()
-
-                if main_text is None:
-                    main_text = post.css('div.field.field-name-field-bullet-points.field-type-text.field-label-hidden div.field-items div.field-item::text').getall()
-                    main_text = ', '.join(main_text) 
 
                 article_info = {
                     'url': url,
                     'date_of_publication': date,
                     'headline': headline,
-                    'main_text': main_text
                 }
+
+                # Check first 10 articles and see if the article that we are trying to scrape is not already in there by its date.
+                # If it finds the articles date is already in posts.json then it should just return.
+                if self.current_posts != "":
+                    for i in range(len(self.current_posts)-1):
+                        if url == self.current_posts[i]['url']:
+                            return
+                # Else we should keep parsing articles.
                 # Goes into the article url and calls the parseArticle method on that article page.
                 next_news = response.urljoin(url)
                 request = scrapy.Request(next_news, callback=self.parseArticle)
@@ -60,11 +77,10 @@ class PostsSpider(scrapy.Spider):
 
                 yield request
 
-        # Go to the next page of articles and call the parse method again for that page.
-
         # Get the next pages url.
         next_page = response.css('li.pager-next a::attr(href)').get()
         # Make sure that a next page does exist.
+        # Go to the next page of articles and call the parse method again for that page.
         if next_page is not None:
             next_page = response.urljoin(next_page)
             # Call the parse method again for the next page.
@@ -75,5 +91,11 @@ class PostsSpider(scrapy.Spider):
         article_info = response.meta['item']
         article_text = response.css('div.clearfix').get()
         article_info['article_text'] = article_text
-
-        yield article_info
+        # If there were already news entries in posts.json then we should append to it.
+        if self.current_posts != "":
+            self.current_posts.insert(0, article_info)
+            with open(self.path_to_json, "w") as news_posts:
+                json.dump(self.current_posts, news_posts)
+                news_posts.close()
+        else:
+            yield article_info
