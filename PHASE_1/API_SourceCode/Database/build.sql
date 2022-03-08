c02e740784498bc24e85438faa2556b48957e415
Nicholas Ramsay <nick@yousponsor.me>
2022-03-08 13:53:32 +1100

    added database setup script

---
+++ PHASE_1/API_SourceCode/Database/build.sql
@@ -0,0 +1,38 @@
+CREATE TABLE Location ( -- contains nullable values eg city, we may not be able to specify an exact city from the text?
+    id INTEGER PRIMARY KEY,
+    country VARCHAR(128) NOT NULL,
+    state VARCHAR(128), -- or province, region, etc
+    city VARCHAR(128), -- or town, village, etc
+);
+CREATE TABLE Time (
+    id INTEGER PRIMARY KEY,
+    year INTEGER NOT NULL,
+    month INTEGER NOT NULL,
+    day INTEGER NOT NULL,
+    hour INTEGER, -- caa be null
+    minute INTEGER
+);
+CREATE TABLE Article ( -- "cluster"
+    id INTEGER PRIMARY KEY,
+    url VARCHAR(512),
+    headline VARCHAR(255),
+    main_text VARCHAR(100000), -- 100 kb is a very large text file
+    date_of_publication DATETIME -- unlike a report we can always scrape the exact date and time
+);
+CREATE TABLE Report ( -- "case"
+    id INTEGER PRIMARY KEY,
+    article_id INTEGER FOREIGN KEY REFERENCES Article(id)
+    location_id INTEGER FOREIGN KEY REFERENCES Location(id),
+    time_id INTEGER FOREIGN KEY REFERENCES Time(id),
+    syndrome VARCHAR(128)
+);
+CREATE TABLE DiseaseCase (
+    id INTEGER PRIMARY KEY,
+    report_id FOREIGN KEY REFERENCES Report(id),
+    name VARCHAR(128),
+);
+CREATE TABLE SyndromeCase ( -- do we need this?
+    id INTEGER PRIMARY KEY,
+    report_id FOREIGN KEY REFERENCES Report(id),
+    name VARCHAR(128)
+);
