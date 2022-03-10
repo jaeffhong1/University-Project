8df63340e22a3484979adae57b407139493fd478
Nicholas Ramsay <nick@yousponsor.me>
2022-03-10 23:20:58 +1100

    added model

--- PHASE_1/API_SourceCode/Database/model/model.py
+++ PHASE_1/API_SourceCode/Database/model/model.py
@@ -58,9 +58,11 @@ class Model:
         self.connection = connection
         
     def InsertArticle(self, url, headline, year, month, day, hour, minute=None):
-        
+        daydate = f"{year}-{"
+        self.conection.cmd_query(f"INSERT INTO EventDate (daydate, hour, minute)")
+        self.connection.cmd_query(f"INSERT INTO Article (url, headline")
     
-    
+
 # test the connector
 if __name__ == '__main__':
     with DBConnection() as dbc:
