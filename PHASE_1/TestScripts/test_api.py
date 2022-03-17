3efe49ed900502e36a1367743b2deb2000e8ff69
Tanya <tanya.wang3@outlook.com>
2022-03-17 14:27:20 +0800

    added tests for invalid date expression and timezone

--- PHASE_1/TestScripts/test_api.py
+++ PHASE_1/TestScripts/test_api.py
@@ -131,6 +131,66 @@ def test_report_invalid_date_range(client):
     assert response.json == expected_response
 
 
+def test_article_invalid_date_expression(client):
+    expected_response = {"message": "Invalid date expression"}
+    # test invalid start_date
+    url = "/article/filter?start_date=2022-02Txx:xx:xx&end_date=2022-03-01Txx:xx:xx&key_terms=outbreak&location=california"
+    response = client.get(url)
+    assert response.status_code == 400
+    assert response.json == expected_response
+    # test invalid end_date
+    url = "/article/filter?start_date=2022-02-10Txx:xx:xx&end_date=2022/03/01Txx:xx:xx&key_terms=outbreak&location=california"
+    response = client.get(url)
+    assert response.status_code == 400
+    assert response.json == expected_response
+
+
+def test_report_invalid_date_expression(client):
+    expected_response = {"message": "Invalid date expression"}
+    # test invalid start_date
+    url = "/report/filter?start_date=2022-02&end_date=2022-03-01Txx:xx:xx&key_terms=outbreak&location=california"
+    response = client.get(url)
+    assert response.status_code == 400
+    assert response.json == expected_response
+    # test invalid end_date
+    url = "/report/filter?start_date=2022-02-20Txx:xx:xx&end_date=2022/03/01Txx:xx:xx&key_terms=outbreak&location=california"
+    response = client.get(url)
+    assert response.status_code == 400
+    assert response.json == expected_response
+
+
+def test_article_invalid_timezone_expression(client):
+    expected_response = {"message": "Invalid timezone expression"}
+    url = "/article/filter?start_date=2022-02-10Txx:xx:xx&end_date=2022-03-01Txx:xx:xx&key_terms=outbreak&location=california&timezone=abcd"
+    response = client.get(url)
+    assert response.status_code == 400
+    assert response.json == expected_response
+    url = "/article/filter?start_date=2022-02-10Txx:xx:xx&end_date=2022-03-01Txx:xx:xx&key_terms=outbreak&location=california&timezone=abcd+1000"
+    response = client.get(url)
+    assert response.status_code == 400
+    assert response.json == expected_response
+    url = "/article/filter?start_date=2022-02-10Txx:xx:xx&end_date=2022-03-01Txx:xx:xx&key_terms=outbreak&location=california&timezone=abcd-1000"
+    response = client.get(url)
+    assert response.status_code == 400
+    assert response.json == expected_response
+
+
+def test_report_invalid_timezone_expression(client):
+    expected_response = {"message": "Invalid timezone expression"}
+    url = "/report/filter?start_date=2022-02-10Txx:xx:xx&end_date=2022-03-01Txx:xx:xx&key_terms=outbreak&location=california&timezone=abcd"
+    response = client.get(url)
+    assert response.status_code == 400
+    assert response.json == expected_response
+    url = "/report/filter?start_date=2022-02-10Txx:xx:xx&end_date=2022-03-01Txx:xx:xx&key_terms=outbreak&location=california&timezone=utc+1000"
+    response = client.get(url)
+    assert response.status_code == 400
+    assert response.json == expected_response
+    url = "/report/filter?start_date=2022-02-10Txx:xx:xx&end_date=2022-03-01Txx:xx:xx&key_terms=outbreak&location=california&timezone=utc-1000"
+    response = client.get(url)
+    assert response.status_code == 400
+    assert response.json == expected_response
+
+
 def test_missing_url(client):
     expected_response = {"message": "Missing required query parameter(s)"}
     url = "/report/from_article_url"
