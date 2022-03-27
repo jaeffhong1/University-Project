81d56623578c0f44893b049f12d389bfd2b6ff38
Mathieu PATUREL <australie.p@gmail.com>
2022-03-27 17:59:08 +1100

    uh ok?

--- PHASE_2/app/src/App.tsx
+++ PHASE_2/app/src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import { Mosaic } from 'react-mosaic-component';
+import 'react-mosaic-component/react-mosaic-component.css';
 import './App.css';
+
+const ELEMENT_MAP: { [viewId: string]: JSX.Element } = {
+  a: <div>Left Window</div>,
+  b: <div>Top Right Window</div>,
+  c: <div>Bottom Right Window</div>,
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+    <Mosaic<string>
+      renderTile={(id) => ELEMENT_MAP[id]}
+      initialValue={{
+        direction: 'row',
+        first: 'a',
+        second: {
+          direction: 'column',
+          first: 'b',
+          second: 'c',
+        },
+        splitPercentage: 40,
+      }}
+      className="mosaic-blueprint-theme bp3-dark"
+      />
     </div>
   );
 }
