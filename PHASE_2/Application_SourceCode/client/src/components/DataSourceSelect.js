178d96ffb3084dd65794044a8b0776bd5bd391b8
nickramsay19 <nickramsay19@gmail.com>
2022-03-28 15:30:40 +1100

    added data source widget w/ info modal,

--- PHASE_2/Application_SourceCode/client/src/components/DataSourceSelect.js
+++ PHASE_2/Application_SourceCode/client/src/components/DataSourceSelect.js
@@ -1,38 +1,104 @@
+import { useState } from 'react';
 import { useLocation } from "react-router-dom";
 
-import { Menu, Dropdown, Button, message, Space, Tooltip } from 'antd';
+import { Menu, Dropdown, Button, message, Tooltip, Checkbox, Modal } from 'antd';
+import { InfoOutlined } from '@ant-design/icons';
 
 export default function BreadCrumb() {
 
+    const dataSources = [
+        {
+            name: "CIDRAP",
+            description: "CIDRAP is a datasource. Here is a description about it."
+        },
+        {
+            name: "Health API",
+            description: "Health API is a datasource. Here is a description about it."
+        }
+    ];
+
     function handleButtonClick(e) {
-        message.info('Click on left button.');
-        console.log('click left button', e);
+        message.info('Select a datasource using the dropdown');
     }
 
     function handleMenuClick(e) {
-        message.info('Click on menu item.');
-        console.log('click', e);
+        //message.info('Click on menu item.');
+        //console.log('click', e);
+    }
+
+    const [dataSourceSelection, setDataSourceSelection] = useState(0);
+    function handleCheckboxChange(id) {
+        setDataSourceSelection(id);
+    }
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+    
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+    
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    const [modalTitle, setModalTitle] = useState("NO TITLE");
+    const [modalText, setModalText] = useState("NO TEXT");
+
+    function handleInfoClick(id) {
+
+        // before showing the modal, set the modal title and description
+        // get the datasource title/description details from the datasource id provided
+        setModalTitle(dataSources[id].name);
+        setModalText(dataSources[id].description);
+        
+        // show the modal
+        showModal();
     }
 
     return (
 
-    <Dropdown.Button 
-        onClick={handleButtonClick} 
-        overlay={(
-            <Menu onClick={handleMenuClick}>
-                <Menu.Item key="1">
-                    1st menu item
-                </Menu.Item>
-                <Menu.Item key="2">
-                    2nd menu item
-                </Menu.Item>
-                <Menu.Item key="3">
-                    3rd menu item
-                </Menu.Item>
-            </Menu>
-        )}
-    >
-        Data Sources
-    </Dropdown.Button>
+        <>
+            <Modal 
+                title={modalTitle} 
+                visible={isModalVisible} 
+                onOk={handleOk}
+                footer={[
+                    <Button key="ok" type="primary" onClick={handleOk}>
+                        Ok
+                    </Button>
+                ]}
+            >
+                <p>{modalText}</p>
+            </Modal>
+
+            <Dropdown.Button 
+                onClick={handleButtonClick} 
+                overlay={(
+                    <Menu onClick={handleMenuClick}>
+                        {
+                            dataSources.map((value, index) => {
+                                return (
+                                    <Menu.Item key={index}>
+                                        <Checkbox onChange={() => handleCheckboxChange(index)} checked={dataSourceSelection == index}>
+                                            <span style={{marginRight: '2em'}}>{value.name}</span>
+
+                                            <Tooltip title="Info">
+                                                <Button style={{float: 'right', position: 'absolute', right: '0.4em'}} shape="circle" size="small" icon={<InfoOutlined />}  onClick={() => handleInfoClick(index)}/>
+                                            </Tooltip>
+                                        </Checkbox>
+                                    </Menu.Item>
+                                )
+                            })
+                        }
+                    </Menu>
+                )}
+            >
+                Data Sources
+            </Dropdown.Button>
+        </>
     );
 }
