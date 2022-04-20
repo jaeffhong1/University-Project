590f0174385d67b86fb4e29f4224c346bd4ccd2c
jaeff <jaeff.hong@education.nsw.gov.au>
2022-04-20 15:58:12 +1000

    Beatiful market

--- PHASE_2/Application_SourceCode/client/src/pages/Marketplace/Marketplace.tsx
+++ PHASE_2/Application_SourceCode/client/src/pages/Marketplace/Marketplace.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { IExternalSource, TExternalSources } from "../Dashboard/DashboardHome/sources/ExternalSource";
+import { AllExternalSourcesPage } from '../../pages/ExternalSourcesPage/AllExternalSourcesPage';
+import { UserExternalSourcesPage } from '../../pages/ExternalSourcesPage/UserExternalSources';
+import { Button, message, Space, Table, Collapse, Typography, Descriptions, List } from "antd";
+
+const { Panel } = Collapse;
+const { Title, Text } = Typography;
 
 interface Props {
     userExternalSources: TExternalSources | null,
@@ -8,7 +14,16 @@ interface Props {
     setExternalSources: (s: {[name: string]: IExternalSource}) => void
 }
 interface State {}
-
+//<UserExternalSourcesPage userExternalSources={this.props.userExternalSources} setUserExternalSources={this.props.setUserExternalSources.bind(this)}/>
 export class Marketplace extends React.Component<Props, State> {
-    
+    render() {
+        if (!this.props.userExternalSources)
+            return <p>Loading external sources, please wait...</p>
+
+
+        return <div style={{padding: '0.5em'}}>
+            <UserExternalSourcesPage userExternalSources={this.props.userExternalSources} setUserExternalSources={this.props.setUserExternalSources.bind(this)}/>
+            <AllExternalSourcesPage userExternalSources={this.props.userExternalSources} externalSources={this.props.externalSources} setExternalSources={this.props.setUserExternalSources.bind(this)}/> 
+        </div>
+    }
 }
